c045be268b2ed72ad1a142de3bdb033b03ed3920
nuzzzen <info@ric.nz>
2024-12-27 11:52:27 +0100

    Remove log to stdout

--- logger_setup.py
+++ logger_setup.py
@@ -2,10 +2,8 @@ import os
 import logging
 
 def setup_logger():
-    log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ups_monitor.log')
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s',
-                        handlers=[logging.FileHandler(log_file),
-                                  logging.StreamHandler()])
+                        handlers=[logging.StreamHandler()])
     logger = logging.getLogger(__name__)
     return logger
